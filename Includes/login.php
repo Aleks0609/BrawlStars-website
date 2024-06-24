660b803fa97cb1a71e4768528710b7389407f159
DosRi574 <ricardodsm5@gmail.com>
2024-06-24 23:58:19 +0200

    update

---
+++ Includes/login.php
@@ -0,0 +1,48 @@
+<?php
+    // Verbindungsaufbau
+    $hostname = '89.58.47.144';
+    $username = 'BrawlstarsUser';
+    $password = 'BrawlstarsPW';
+    $dbname = 'dbBrawlstars';
+
+    // Verbindungsversuch zur MySQL-Datenbank
+    $connection = mysqli_connect($hostname, $username, $password, $dbname);
+
+    if (!$connection) {
+        die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
+    }
+
+    // Vorbereitung des SELECT-Statements
+    $sql = "SELECT dtName, dtEmail, dtPassword FROM tblUser WHERE dtName LIKE 'username' AND dtEmail LIKE 'email' AND dtPassword LIKE 'password'";
+
+    // Ausführung des SELECT-Statements
+    $result = mysqli_query($connection, $sql);
+
+    // Überprüfen ob der Query richtig ist.
+    if ($result) {
+        echo "Dein Query wurde erfolgreich gesendet und verarbeitet.";
+    } else {
+        echo "Fehler beim ausführen deines Queries: " . mysqli_error($connection);
+    }
+
+    echo mysqli_num_rows($result);
+
+    // Datenverarbeitung
+    if (mysqli_num_rows($result) > 0) {
+        // Anzahl der erhaltenen Datensetze.
+        $numRows = mysqli_num_rows($result);
+        // Daten aus jeder Reihe ausgeben (Pfankuchen vom Stapel nehmen)
+        for ($i=0; $i < $numRows; $i++) { 
+            $row = mysqli_fetch_assoc($result);
+            echo "id: " . $row["idUser"]. " - Text: " . $row["dtName"]. "";
+        }
+    } else {
+        echo "0 Ergebnisse gefunden";
+    }
+
+    // Resultat freisetzen (Den Teller wegräumen)
+    mysqli_free_result($result);
+
+    // Schließen der Verbindung
+    mysqli_close($connection);
+?>
---
+++ Includes/register.php
@@ -0,0 +1,30 @@
+<?php
+    // Verbindungsaufbau
+    $hostname = '89.58.47.144';
+    $username = 'BrawlstarsUser';
+    $password = 'BrawlstarsPW';
+    $dbname = 'dbBrawlstars';
+
+    // Verbindungsversuch zur MySQL-Datenbank
+    $connection = mysqli_connect($hostname, $username, $password, $dbname);
+
+    if (!$connection) {
+        die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
+    }
+
+    // Vorbereitung des INSERT-Statements
+    // Nimm an, dass $text die Nachricht ist, die du einfügen möchtest.
+    // In einem realen Szenario solltest du $text zuerst validieren und bereinigen.
+    $text = "Deine Nachricht hier einfügen";
+    $sql = "INSERT INTO deineTabelle (nachrichtSpalte) VALUES ('".$text."')";
+
+    // Ausführung des INSERT-Statements
+    if (mysqli_query($connection, $sql)) {
+        echo "Nachricht erfolgreich eingefügt";
+    } else {
+        echo "Fehler beim Einfügen der Nachricht: " . mysqli_error($connection);
+    }
+
+    // Schließen der Verbindung
+    mysqli_close($connection);
+?>
